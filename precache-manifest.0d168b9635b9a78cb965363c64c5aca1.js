3a8f662bcb71e063b050234ffab2fbb3290ee319
OyukiCV <oyukivel@hotmail.com>
2019-09-30 15:15:54 -0500

    Updates

---
+++ precache-manifest.0d168b9635b9a78cb965363c64c5aca1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd45e55f84bde9162666a0a2a1b083d",
+    "url": "/MEX008-FE-Burger-Queen/index.html"
+  },
+  {
+    "revision": "036c86de05e84aef8516",
+    "url": "/MEX008-FE-Burger-Queen/static/css/main.706f2937.chunk.css"
+  },
+  {
+    "revision": "8f4d52b6934f7e6d7ba9",
+    "url": "/MEX008-FE-Burger-Queen/static/js/2.6b7908a8.chunk.js"
+  },
+  {
+    "revision": "036c86de05e84aef8516",
+    "url": "/MEX008-FE-Burger-Queen/static/js/main.194bc888.chunk.js"
+  },
+  {
+    "revision": "d2c7ca4ba3a2048f236c",
+    "url": "/MEX008-FE-Burger-Queen/static/js/runtime~main.c891d290.js"
+  }
+]);
