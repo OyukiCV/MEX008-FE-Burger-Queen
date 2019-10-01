dad4c18a4704e08db3f69635ade08437be6d0e97
OyukiCV <oyukivel@hotmail.com>
2019-10-01 00:38:51 -0500

    Updates

--- precache-manifest.a2e51d860e829accd2bac6e9a5267d55.js
+++ precache-manifest.a2e51d860e829accd2bac6e9a5267d55.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4fc6684894b909ee6e7ddd14304137f8",
+    "revision": "178bf4461c2d96b6cddcf63cdb524deb",
     "url": "/MEX008-FE-Burger-Queen/index.html"
   },
   {
-    "revision": "4ca712787f06f776089b",
-    "url": "/MEX008-FE-Burger-Queen/static/css/main.bc8127f5.chunk.css"
+    "revision": "0153620d988e055bbf08",
+    "url": "/MEX008-FE-Burger-Queen/static/css/main.c4f1b1e8.chunk.css"
   },
   {
     "revision": "6f6a00ca27b0b65f556d",
     "url": "/MEX008-FE-Burger-Queen/static/js/2.a72691cf.chunk.js"
   },
   {
-    "revision": "4ca712787f06f776089b",
-    "url": "/MEX008-FE-Burger-Queen/static/js/main.c3828ad3.chunk.js"
+    "revision": "0153620d988e055bbf08",
+    "url": "/MEX008-FE-Burger-Queen/static/js/main.b6ada03d.chunk.js"
   },
   {
     "revision": "d2c7ca4ba3a2048f236c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MEX008-FE-Burger-Queen/precache-manifest.4cde92c2e07ebfbd23f8ac31a251d078.js"
+  "/MEX008-FE-Burger-Queen/precache-manifest.a2e51d860e829accd2bac6e9a5267d55.js"
 );
 
 self.addEventListener('message', (event) => {
