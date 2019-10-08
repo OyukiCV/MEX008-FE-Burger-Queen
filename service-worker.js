23c6a6d8443421d87e97b2f95c5b565dbe78c9c6
OyukiCV <oyukivel@hotmail.com>
2019-10-08 14:48:25 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MEX008-FE-Burger-Queen/precache-manifest.c535b8ab678fb4623dd68c4c8e87f0e1.js"
+  "/MEX008-FE-Burger-Queen/precache-manifest.12070a344fcf8cc5320e60ba32875c3d.js"
 );
 
 self.addEventListener('message', (event) => {
